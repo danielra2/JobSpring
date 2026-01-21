4e633699fbca55643096de0dff53208abd4df4b7
Radoi Daniel <radoidaniel1707@gmail.com>
2026-01-21 11:29:06 +0200

    .

--- src/test/java/mycode/jobspring/user/controller/UserControllerIT.java
+++ src/test/java/mycode/jobspring/user/controller/UserControllerIT.java
@@ -1,8 +1,9 @@
 package mycode.jobspring.user.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import mycode.jobspring.masina.models.Masina;
+import mycode.jobspring.user.dtos.UserDto;
 import mycode.jobspring.user.dtos.UserListResponse;
+import mycode.jobspring.user.dtos.UserResponse;
 import mycode.jobspring.user.models.User;
 import mycode.jobspring.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,79 +11,68 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class UserControllerIT {
+
     @Autowired
     private MockMvc mockMvc;
+
     @Autowired
     private UserRepository userRepository;
+
     @Autowired
     private ObjectMapper objectMapper;
 
     @BeforeEach
     void setup() {
+
         userRepository.deleteAll();
     }
 
     @Test
+    void createUserEndpointPersistsEntity() throws Exception {
 
-    void getAllUserReturnsResults() throws Exception {
-//        userRepository.saveAll(List.of(user1,user2));
-        Set<Masina> setMasini1 = new HashSet<>();
+        UserDto userDto = new UserDto("Popescu", "Ion", 30, new HashSet<>());
 
-        Masina masina1=new Masina("Audi","A4",2000,200000,null);
-        Masina masina2=new Masina("Bmw","A4",2000,200000,null);
-        Masina masina3=new Masina("Opel","A4",2000,200000,null);
-        Masina masina4=new Masina("Ford","A4",2000,200000,null);
-        Masina masina5=new Masina("Peugeot","A4",2000,200000,null);
+        MvcResult result = mockMvc.perform(post("/api/job/add").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsBytes(userDto))).andExpect(status().isCreated()).andReturn();
 
-        setMasini1.add(masina1);
-        setMasini1.add(masina2);
-        setMasini1.add(masina3);
-        setMasini1.add(masina4);
-        setMasini1.add(masina5);
+        UserResponse response = objectMapper.readValue(result.getResponse().getContentAsByteArray(),UserResponse.class
+        );
 
-        User user1 = new User(1L, "Daniel", "Ion", 22, setMasini1);
+        assertThat(response.nume()).isEqualTo("Popescu");
+        assertThat(response.prenume()).isEqualTo("Ion");
 
-        Set<Masina> setMasini2 = new HashSet<>();
+        assertThat(userRepository.findByNumeAndPrenume("Popescu", "Ion")).isPresent();
+    }
 
+    @Test
+    void getAllUsersReturnsList() throws Exception {
 
-        Masina masina6 = new Masina("Mercedes", "C-Class", 2021, 35000, null);
-        Masina masina7 = new Masina("Toyota", "Corolla", 2022, 22000, null);
-        Masina masina8 = new Masina("Volkswagen", "Golf", 2019, 15000, null);
-        Masina masina9 = new Masina("Tesla", "Model 3", 2023, 42000, null);
-        Masina masina10 = new Masina("Dacia", "Logan", 2020, 9500, null);
+        User user = new User();
+        user.setNume("Daniel");
+        user.setPrenume("Test");
+        user.setVarsta(22);
+        user.setMasini(new HashSet<>());
+        userRepository.save(user);
 
-        setMasini2.add(masina6);
-        setMasini2.add(masina7);
-        setMasini2.add(masina8);
-        setMasini2.add(masina9);
-        setMasini2.add(masina10);
-
-        User user2=new User(2L,"Popa","Viorel",88,setMasini2);
-
-
-
-        MvcResult result=mockMvc.perform(get("/api/job")).andExpect(status().isOk()).andReturn();
-
-        UserListResponse response=objectMapper.readValue(result.getResponse().getContentAsByteArray(),UserListResponse.class);
-        assertThat(response.userList().size()).isEqualTo(2);
-
-
-
+        MvcResult result = mockMvc.perform(get("/api/job").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+        UserListResponse response = objectMapper.readValue(result.getResponse().getContentAsByteArray(),UserListResponse.class);
+        assertThat(response.userList()).hasSize(1);
+        assertThat(response.userList().get(0).nume()).isEqualTo("Daniel");
     }
 }
